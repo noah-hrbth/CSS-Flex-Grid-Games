d5082fff4bf45ab67287ff908cf368e42e9ffa4b
Noah Harborth <noah.harborth@study.hs-duesseldorf.d>
2021-12-11 15:33:39 +0100

    express und docker aufgesetzt Co-authored-by: Ahriox <Ahriox@users.noreply.github.com>

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -13,13 +13,13 @@ const getCookie = (cookieName = "isOnLevel") => {
   let cookieArray = decodedCookie.split(";");
 
   for (let i = 0; i < cookieArray.length; i++) {
-	let cookie = cookieArray[i];
-	while (cookie.charAt(0) === " ") {
-	  cookie = cookie.substring(1);
-	}
-	if (cookie.indexOf(name) === 0) {
-	  return cookie.substring(name.length, cookie.length);
-	}
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === " ") {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
   }
   return "";
 };
