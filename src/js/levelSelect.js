fbd48f8f55dcecbc5cf6f6167c9081542d7a4a81
Noah Harborth <noah.harborth@study.hs-duesseldorf.d>
2021-12-20 01:42:21 +0100

    changed styling and animation, kinda responsive

--- src/js/levelSelect.js
+++ src/js/levelSelect.js
@@ -6,13 +6,20 @@ levelSelectBtn.addEventListener("click", () => {
     levelSelectLevel.classList.remove("level-select-level--active");
 
     levelSelectLevel.classList.remove("swing-in-left-fwd");
-    levelSelectLevel.classList.add("swing-out-left-fwd");
+    levelSelectLevel.classList.add("hinge-out-tl");
   } else {
     levelSelectLevel.classList.add("level-select-level--active");
 
-    levelSelectLevel.classList.remove("swing-out-left-fwd");
+    levelSelectLevel.classList.remove("hinge-out-tl");
     levelSelectLevel.classList.add("swing-in-left-fwd");
+  }
 
-    levelSelectLevel.style.display = "grid";
+  if (levelSelectLevel.classList.contains("hinge-out-tl")) {
+    const watersplashImageLevelSelect = document.querySelector(
+      ".watersplash-image-level-select"
+    );
+    setTimeout(() => {
+      watersplashImageLevelSelect.src = "/assets/images/watersplash-level-select.gif";
+    }, 1800);
   }
 });
--- src/js/settings.js
+++ src/js/settings.js
@@ -5,14 +5,22 @@ settingsBtn.addEventListener("click", () => {
   if (settingsContent.classList.contains("settings-content--active")) {
     settingsContent.classList.remove("settings-content--active");
 
-    settingsContent.classList.remove("flip-in-hor-bottom");
-    settingsContent.classList.add("flip-out-hor-top");
+    settingsContent.classList.remove("swing-in-top-fwd");
+    settingsContent.classList.add("hinge-out-top");
   } else {
     settingsContent.classList.add("settings-content--active");
 
-    settingsContent.classList.remove("flip-out-hor-top");
-    settingsContent.classList.add("flip-in-hor-bottom");
-
-    settingsContent.style.display = 'flex';
+    settingsContent.classList.remove("hinge-out-top");
+    settingsContent.classList.add("swing-in-top-fwd");
   }
+
+  if (settingsContent.classList.contains("hinge-out-top")) {
+    const watersplashImageSettings = document.querySelector('.watersplash-image-settings');
+    setTimeout(() => {
+      watersplashImageSettings.src = "/assets/images/watersplash-settings.gif";
+    }, 1800);
+  }
+
 });
+
+
