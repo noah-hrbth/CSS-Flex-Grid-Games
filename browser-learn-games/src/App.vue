33c25c92cbec6548b9aaee905c0770767ec19115
Noah Harborth <noah.harborth@study.hs-duesseldorf.d>
2022-01-08 23:12:04 +0100

    added the settings styling and also a back to home btn

--- browser-learn-games/src/App.vue
+++ browser-learn-games/src/App.vue
@@ -1,108 +1,131 @@
 <template>
-    <div>
-        <div v-if="this.$store.state.game.currentGame == 0">
-            <HomeScreen />
+  <div>
+    <div v-if="this.$store.state.game.currentGame == 0">
+      <HomeScreen />
+    </div>
+    <div class="main-container" v-else>
+      <div class="left">
+        <div class="game-logo">
+          <h1>HSD Flex Pirate</h1>
+          <button class="back-to-home">
+            <a
+              @click="
+                this.$store.state.game.currentGame = 0;
+                this.$store.state.game.currentLevel = 0;
+              "
+              class="back-to-home-link"
+              >Zurück zur Übersicht</a
+            >
+          </button>
         </div>
-        <div class="main-container" v-else>
-            <div class="left">
-                <div class="game-logo">HSD Flex Pirate</div>
-                <div class="level-select">
-                    <button class="level-select-btn" @click="openLevelSelect()">
-                        <img
-                        :src="levelSelectImage"
-                        alt="Level select"
-                        class="level-icon"
-                        />
-                    </button>
-                    <div class="level-select-level" :class="{ 'level-select-level--active': this.levelSelectOpen }">
-                        <div v-if="this.$store.state.game.currentGame == 1">
-                            <div v-for="level in this.$store.state.levelData.flex" :key="level.levelNr" v-on:click="goToLevel(1, (level.levelNr - 1))">
-                                <span class="level-nr">{{ level.levelNr }}</span>
-                            </div>
-                        </div>
-                        <div v-if="this.$store.state.game.currentGame == 2">
-                            <div v-for="level in this.$store.state.levelData.grid" :key="level.levelNr" v-on:click="goToLevel(2, (level.levelNr - 1))">
-                                <span class="level-nr">{{ level.levelNr }}</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <Settings />
-
-                <Task />
-
-                <Input />
-                
-                <img src="" alt="" class="watersplash-image-level-select">
-                <img src="" alt="" class="watersplash-image-settings">
-            </div>
-
+        <div class="level-select">
+          <button class="level-select-btn" @click="openLevelSelect()">
+            <img
+              :src="levelSelectImage"
+              alt="Level select"
+              class="level-icon"
+            />
+          </button>
+          <div
+            class="level-select-level"
+            :class="{ 'level-select-level--active': this.levelSelectOpen }"
+          >
             <div v-if="this.$store.state.game.currentGame == 1">
-                <FlexGame :level-data="this.currentGameData.levelNr" />
+              <div
+                v-for="level in this.$store.state.levelData.flex"
+                :key="level.levelNr"
+                v-on:click="goToLevel(1, level.levelNr - 1)"
+              >
+                <span class="level-nr">{{ level.levelNr }}</span>
+              </div>
             </div>
             <div v-if="this.$store.state.game.currentGame == 2">
-                <GridGame :level-data="this.currentGameData.levelNr" />
+              <div
+                v-for="level in this.$store.state.levelData.grid"
+                :key="level.levelNr"
+                v-on:click="goToLevel(2, level.levelNr - 1)"
+              >
+                <span class="level-nr">{{ level.levelNr }}</span>
+              </div>
             </div>
+          </div>
         </div>
+
+        <Settings />
+
+        <Task />
+
+        <Input />
+
+        <img src="" alt="" class="watersplash-image-level-select" />
+        <img src="" alt="" class="watersplash-image-settings" />
+      </div>
+
+      <div v-if="this.$store.state.game.currentGame == 1">
+        <FlexGame :level-data="this.currentGameData.levelNr" />
+      </div>
+      <div v-if="this.$store.state.game.currentGame == 2">
+        <GridGame :level-data="this.currentGameData.levelNr" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import './assets/css/_variables.css'
-import './assets/css/_animation.css'
-import './assets/css/_reset.css'
+import "./assets/css/_variables.css";
+import "./assets/css/_animation.css";
+import "./assets/css/_reset.css";
 
-import FlexGame from './components/FlexGame.vue'
-import GridGame from './components/GridGame.vue'
-import HomeScreen from './components/HomeScreen.vue'
-import Settings from './components/Settings.vue'
-import Task from './components/Task.vue'
-import Input from './components/Input.vue'
+import FlexGame from "./components/FlexGame.vue";
+import GridGame from "./components/GridGame.vue";
+import HomeScreen from "./components/HomeScreen.vue";
+import Settings from "./components/Settings.vue";
+import Task from "./components/Task.vue";
+import Input from "./components/Input.vue";
 
-import LevelSelectImage from './assets/images/level-select.png'
+import LevelSelectImage from "./assets/images/level-select.png";
 
 export default {
-    name: 'App',
-    components: {
-        FlexGame,
-        GridGame,
-        HomeScreen,
-        Settings,
-        Task,
-        Input
+  name: "App",
+  components: {
+    FlexGame,
+    GridGame,
+    HomeScreen,
+    Settings,
+    Task,
+    Input,
+  },
+  methods: {
+    goToLevel(game, level) {
+      this.currentGame = game;
+      this.currentGameData.levelNr = level;
+      this.$store.state.game.currentGame = game;
+      this.$store.state.game.currentLevel = level;
+      this.$store.state.editor.editorInput = "";
     },
-    methods: {
-        goToLevel(game, level) {
-            this.currentGame = game;
-            this.currentGameData.levelNr = level;
-            this.$store.state.game.currentGame = game;
-            this.$store.state.game.currentLevel = level;
-            this.$store.state.editor.editorInput = "";
-        },
 
-        openLevelSelect() {
-            if(!this.levelSelectOpen){
-                this.levelSelectOpen = true
-                this.levelSelectClassArray.push("level-select-level--active")
-            } else {
-                this.levelSelectOpen = false
-                this.levelSelectClassArray = []
-            }
-        }
+    openLevelSelect() {
+      if (!this.levelSelectOpen) {
+        this.levelSelectOpen = true;
+        this.levelSelectClassArray.push("level-select-level--active");
+      } else {
+        this.levelSelectOpen = false;
+        this.levelSelectClassArray = [];
+      }
     },
-    data: () => ({
-        currentGame: 0,
-        currentGameData: {
-            levelNr: 0,
-            currentPosition: {x: 0, y: 0}
-        },
+  },
+  data: () => ({
+    currentGame: 0,
+    currentGameData: {
+      levelNr: 0,
+      currentPosition: { x: 0, y: 0 },
+    },
 
-        levelSelectOpen: false,
-        levelSelectClassArray: [],
-        levelSelectImage: LevelSelectImage,
-    })
-}
+    levelSelectOpen: false,
+    levelSelectClassArray: [],
+    levelSelectImage: LevelSelectImage,
+  }),
+};
 </script>
 
 <style>
@@ -113,8 +136,8 @@ body {
 }
 
 #app {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
 .main-container {
@@ -143,7 +166,24 @@ body {
 .game-logo {
   grid-area: Logo;
   font-family: var(--font-heading);
-  font-size: 2rem;
+}
+
+.back-to-home {
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  margin-top: 1rem;
+  cursor: pointer;
+  border: solid 3px var(--color-light);
+}
+
+.back-to-home-link {
+  color: var(--color-light);
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+.back-to-home:hover .back-to-home-link {
+  text-decoration: underline;
 }
 
 .level-select {
--- browser-learn-games/src/components/Settings.vue
+++ browser-learn-games/src/components/Settings.vue
@@ -1,46 +1,58 @@
 <template>
-    <div class="settings">
-        <button class="settings-btn" @click="this.settingsOpen = !this.settingsOpen">
-            <img
-            :src="settingsIconImage"
-            alt="Settings"
-            class="settings-icon"
-            />
-        </button>
-        <ul class="settings-content" :class="{ 'settings-content--active': this.settingsOpen }">
-            <br>
-            <section>
-                <p id="labelLanguage" class="Sprachenauswahl">Sprache</p>
-                <select id="language">
-                  <option value="de">Deutsch</option>
-                  <option value="en">Englisch</option>
-                </select>
-            </section>
-            <li class="setting-options"> Lautstärke</li>
-            <li class="setting-options"> Settings3</li>
-        </ul>
-    </div>
+  <div class="settings">
+    <button
+      class="settings-btn"
+      @click="this.settingsOpen = !this.settingsOpen"
+    >
+      <img :src="settingsIconImage" alt="Settings" class="settings-icon" />
+    </button>
+    <ul
+      class="settings-content"
+      :class="{ 'settings-content--active': this.settingsOpen }"
+    >
+      <li class="settings-option language-change">
+        <span>Sprache</span>
+        <select id="language" class="language-change__dropdown">
+          <option value="de">Deutsch</option>
+          <option value="en">Englisch</option>
+        </select>
+      </li>
+      <li class="settings-option hint-change">
+        <span>Hinweise</span>
+        <span class="hint-change__switchtext"> an </span>
+        <label class="hint-change__switch">
+          <input type="checkbox" id="checkbox" />
+          <span class="slider round"></span>
+        </label>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import SettingsIconImage from '../assets/images/settings-icon.png'
+import SettingsIconImage from "../assets/images/settings-icon.png";
 
 export default {
-    name: 'Settings',
-    data: () => ({
-        settingsIconImage: SettingsIconImage,
-        settingsOpen: false,
-    }),
-    methods: {
-
-    },
-}
+  name: "Settings",
+  data: () => ({
+    settingsIconImage: SettingsIconImage,
+    settingsOpen: false,
+  }),
+  methods: {},
+};
 </script>
 
 <style>
 .settings-btn {
   height: 100%;
   cursor: pointer;
+  border-radius: 100%;
+  box-shadow: inset 0px 57px 0px -18px rgba(0, 0, 0, 0.24);
+}
+
+.settings-btn:active {
+  transform: scale(0.95);
+  box-shadow: inset 0px 57px 0px -18px rgba(0, 0, 0, 0.103);
 }
 
 .settings-icon {
@@ -55,18 +67,98 @@ export default {
   background-size: cover;
   height: 6.625rem;
   width: 13.313rem;
-  align-items: center;
   flex-direction: column;
   z-index: -1;
   pointer-events: none;
-}
-
-.setting-options {
- cursor: pointer;
+  gap: 0.5rem;
+  padding-left: 0.5rem;
+  padding-top: 1.5rem;
 }
 
 .settings-content--active {
   opacity: 1;
   pointer-events: all;
+}
+
+.settings-option {
+  display: flex;
+  justify-content: center;
+}
+
+.language-change {
+  gap: 1rem;
+}
+
+.language-change__dropdown {
+  padding: 0.2rem;
+  background: var(--color-grey);
+  color: var(--color-light);
+  border: 1px solid transparent;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+.hint-change__switchtext {
+  flex-basis: 20%;
+  text-align: center;
+}
+
+.hint-change__switch {
+  position: relative;
+  display: inline-block;
+  width: 3rem;
+  height: 1.5rem;
+}
+
+/* Hide default HTML checkbox */
+.hint-change__switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: var(--color-grey);
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 15px;
+  width: 15px;
+  left: 5px;
+  bottom: 4px;
+  background-color: var(--color-light);
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: var(--color-lightgrey);
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px var(--color-lightgrey);
+}
+
+input:checked + .slider:before {
+  transform: translateX(23px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 25rem;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
