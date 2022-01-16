2e69c72b058a2330cd0b408178f9c3c5ffc20c85
Ahriox <school@deadneko.de>
2022-01-16 17:13:43 +0100

    added more player obj

--- browser-learn-games/src/components/FlexGame.vue
+++ browser-learn-games/src/components/FlexGame.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="right">
     <div class="inner-right-target">
-      <div class="target-container" id="target-container">
+      <div class="target-container" id="target-container" :style="this.getLevelById(this.$store.state.game.currentLevel).solution">
         <img
           class="image target-treasure animate__animated animate__bounce animate__slow animate__delay-1s animate__infinite"
           :src="treasureImage"
+          alt="Ziel Truhe"
+        />
+      </div>
+      <div class="target-container" id="target-container" :style="[this.getLevelById(this.$store.state.game.currentLevel).solution, this.getLevelById(this.$store.state.game.currentLevel).playerAmount > 1 ? '' : 'display:none;']">
+        <img
+          class="image target-treasure animate__animated animate__bounce animate__slow animate__delay-1s animate__infinite"
+          :src="treasureBlueImage"
+          alt="Ziel Truhe"
+        />
+      </div>
+      <div class="target-container" id="target-container" :style="[this.getLevelById(this.$store.state.game.currentLevel).solution, this.getLevelById(this.$store.state.game.currentLevel).playerAmount > 2 ? '' : 'display:none;']">
+        <img
+          class="image target-treasure animate__animated animate__bounce animate__slow animate__delay-1s animate__infinite"
+          :src="treasureYellowImage"
           alt="Ziel Truhe"
         />
       </div>
@@ -20,13 +34,31 @@
           alt="Player Pirate"
         />
       </div>
+      <div class="player-container" id="player-container" :style="this.getLevelById(this.$store.state.game.currentLevel).playerAmount > 1 ? '' : 'display:none;'">
+        <img
+          class="image player-pirate"
+          :src="playerPirateBlueImage"
+          alt="Player Pirate"
+        />
+      </div>
+      <div class="player-container" id="player-container" :style="this.getLevelById(this.$store.state.game.currentLevel).playerAmount > 2 ? '' : 'display:none;'">
+        <img
+          class="image player-pirate"
+          :src="playerPirateYellowImage"
+          alt="Player Pirate"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import PlayerPirateImage from "../assets/images/pirate-player.png";
+import PlayerPirateBlueImage from "../assets/images/pirate-player-blue.png";
+import PlayerPirateYellowImage from "../assets/images/pirate-player-yellow.png";
 import TreasureImage from "../assets/images/treasure.png";
+import TreasureBlueImage from "../assets/images/treasure-blue.png";
+import TreasureYellowImage from "../assets/images/treasure-yellow.png";
 import WoodImage from "../assets/images/wood.jpg";
 
 const gameId = 1;
@@ -51,7 +83,11 @@ export default {
   // },
   data: () => ({
     playerPirateImage: PlayerPirateImage,
+    playerPirateBlueImage: PlayerPirateBlueImage,
+    playerPirateYellowImage: PlayerPirateYellowImage,
     treasureImage: TreasureImage,
+    treasureBlueImage: TreasureBlueImage,
+    treasureYellowImage: TreasureYellowImage,
     woodImage: WoodImage,
 
     gameId: gameId,
@@ -112,8 +148,6 @@ export default {
   height: 10rem;
   width: 10rem;
   display: flex;
-  align-items: center;
-  justify-content: center;
 }
 
 .target-treasure {
